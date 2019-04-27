18b7093341d97aefbd7899127e783a71a8646457
Kamil Smolik <ksmolik@outlook.com>
2019-04-27 14:03:35 +0200

    add user re[ository interface

--- src/RssReaders.Core/Repositories/IUserRepository.cs
+++ src/RssReaders.Core/Repositories/IUserRepository.cs
@@ -1,7 +1,16 @@
+using System;
+using System.Threading.Tasks;
+using RssReaders.Core.Model;
+
 namespace RssReaders.Core.Repositories
 {
     public interface IUserRepository
     {
-         
+        Task<User> GetAsync(Guid id); 
+        Task<User> GetAsync(string email);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+
     }
 }
