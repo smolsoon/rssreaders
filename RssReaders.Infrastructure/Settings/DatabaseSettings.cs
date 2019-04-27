0638e857ddfbb0a5c4885f69453fccd1de137eb2
Kamil Smolik <ksmolik@outlook.com>
2019-04-27 13:56:38 +0200

    Initial commit

--- RssReaders.Infrastructure/Settings/DatabaseSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace RssReaders.Infrastructure.Settings
-{
-    public class DatabaseSettings
-    {
-        public string ConnectionString { get; set; }
-        public string Database { get; set; }
-    }
-}
---
+++ src/RssReaders.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RssReaders.Core
+{
+    public class Class1
+    {
+    }
+}
